8e8f98ade5cfbc0ea81381b988898f8eef4d48e5
szaciooo <szatkowskipawel@protonmail.com>
2025-06-13 23:04:11 +0200

    commit_06_postac_grafika_sterowanie

--- include/Player.h
+++ include/Player.h
@@ -1,20 +1,30 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
+// Klasa reprezentująca gracza sterowanego przez użytkownika
 class Player {
 public:
     Player();
 
-    void update(float deltaTime);
-    void render(sf::RenderWindow& window);
+    void update(float deltaTime);         // Aktualizacja pozycji i animacji
+    void render(sf::RenderWindow& window); // Renderowanie postaci
 
 private:
-    sf::Texture texture;
+    void handleInput();        // Obsługa klawiatury
+    void loadTextures();       // Ładowanie grafik
+    void updateAnimation();    // Animacja (jeszcze uproszczona)
+
+    sf::Texture walkTexture;
     sf::Sprite sprite;
 
     sf::Vector2f position;
     sf::Vector2f velocity;
     float speed = 100.f;
 
-    void handleInput();
+    enum Direction { Down, Left, Right, Up } direction = Down;
+    int currentFrame = 0;
+    float frameDuration = 0.15f;
+    sf::Clock animationClock;
+
+    bool moving = false;
 };
--- src/Player.cpp
+++ src/Player.cpp
@@ -1,32 +1,78 @@
 #include "Player.h"
-#include <algorithm>
+#include <iostream>
 
 Player::Player() {
-    position = {700.f, 500.f};
+    position = sf::Vector2f(700.f, 500.f); // Startowa pozycja
+    loadTextures();
 
-    if (!texture.loadFromFile("assets/player/BODY_male_walk.png")) {
-        // W razie problemu załaduj awaryjny kolor lub sprite
-    }
-
-    sprite.setTexture(texture);
-    sprite.setTextureRect(sf::IntRect(0, 0, 64, 64));
+    sprite.setTexture(walkTexture);
+    sprite.setTextureRect(sf::IntRect(0, 0, 64, 64)); // Pierwsza klatka
     sprite.setPosition(position);
+}
+
+void Player::loadTextures() {
+    if (!walkTexture.loadFromFile("assets/player/BODY_male_walk.png")) {
+        std::cerr << "Nie udało się załadować tekstury chodzenia gracza!\n";
+    }
 }
 
 void Player::handleInput() {
     velocity = {0.f, 0.f};
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) velocity.y -= speed;
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) velocity.y += speed;
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) velocity.x -= speed;
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) velocity.x += speed;
+    moving = false;
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+        velocity.x -= speed;
+        direction = Left;
+        moving = true;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+        velocity.x += speed;
+        direction = Right;
+        moving = true;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+        velocity.y -= speed;
+        direction = Up;
+        moving = true;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+        velocity.y += speed;
+        direction = Down;
+        moving = true;
+    }
 }
 
 void Player::update(float deltaTime) {
     handleInput();
     position += velocity * deltaTime;
+
+    // Zapobiegamy wychodzeniu poza ekran (ok. 64px szerokości postaci)
     position.x = std::clamp(position.x, 0.f, 1536.f - 64.f);
     position.y = std::clamp(position.y, 0.f, 1024.f - 64.f);
+
     sprite.setPosition(position);
+    updateAnimation();
+}
+
+void Player::updateAnimation() {
+    if (animationClock.getElapsedTime().asSeconds() > frameDuration) {
+        currentFrame = (currentFrame + 1) % 6; // 6 klatek
+        animationClock.restart();
+    }
+
+    int row = 0;
+    switch (direction) {
+    case Down: row = 0; break;
+    case Left: row = 1; break;
+    case Right: row = 2; break;
+    case Up: row = 3; break;
+    }
+
+    if (moving) {
+        sprite.setTextureRect(sf::IntRect(currentFrame * 64, row * 64, 64, 64));
+    } else {
+        sprite.setTextureRect(sf::IntRect(0, row * 64, 64, 64));
+    }
 }
 
 void Player::render(sf::RenderWindow& window) {
